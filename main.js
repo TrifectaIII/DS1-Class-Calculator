08f5c4eaccacb8203a1ec8f0b96d0742bff236b9
Dakota Madden-Fong <maddenfong@gmail.com>
2019-09-26 22:55:48 -0700

    removal of slc stuff

--- main.js
+++ main.js
@@ -1,119 +1,61 @@
 //formats number with commas
 function formatNum(num) {
-    string = num.toString();
-    newstring = '';
+    before = num.toString();
+    after = '';
     counter = 0;
-    for (let i = string.length - 1; i >= 0; i--) {
+    for (let i = before.length - 1; i >= 0; i--) {
         if (counter == 3) {
             counter = 0;
-            newstring = ',' + newstring;
+            after = ',' + after;
         }
         counter += 1;
-        newstring = string[i] + newstring;
+        after = before[i] + after;
     }
-    return newstring;
+    return after;
 }
 
-//calculates the total number to get from start to goal level
-function calcSouls(start, goal) {
-    var total = 0;
-    var levels = {};
-    var level;
-
-    for (let i = start + 1; i <= goal; i++) {
-        level = values[i]; //from values.js
-        levels[i] = level;
-        total += level;
-    }
-    return { total: total, levels: levels };
-}
 
 //generates output 
-function genOutput(div, start, goal) {
-    if (start < 1) {
-        start = 1;
-    }
-    if (goal > 715) {
-        goal = 715;
-    }
-    var souls = calcSouls(start, goal);
-    var total = souls.total;
-    var levels = souls.levels;
-
-    if (total > 0) {
-        var message = '<p>To get from SL <b>' + start.toString() + '</b> to SL <b>' +
-            goal.toString() + '</b>, it will take <b>' + formatNum(total) +
-            '</b> Souls.</p>';
-
-        var table = '';
-
-        if (Object.keys(levels).length > 1) {
-            table += `<hr class='output_rule'>
-                    <table>
-                    <thead>
-                    <tr>
-                        <th>Level</th>
-                        <th>Souls</th>
-                        <th>Total</th>
-                    </tr>
-                    </thead>
-                    <tbody>`;
-
-            var cumulative = 0;
-            for (let level in levels) {
-                cumulative += levels[level];
-                table += '<tr><th>' +
-                    level.toString() + '</th><td>' +
-                    levels[level].toString() + '</td><td>' +
-                    cumulative.toString() + '</td></tr>';
-            }
-
-            table += '</tbody></table>'
-        }
-
-        div.innerHTML = message + table;
-    } else {
-        div.innerHTML = '';
-    }
+function genOutput(div) {
+    div.innerHTML = 'genOutput() not implemented yet.';
 }
 
 function clearOutput(div) {
     div.innerHTML = '';
 }
 
-var start_level = document.querySelector('.start_level');
-var goal_level = document.querySelector('.goal_level');
+var vitality_goal = document.querySelector('.vitality_goal');
+var attunement_goal = document.querySelector('.attunement_goal');
 var output = document.querySelector('.output');
 
-var started = false;
 
-var startval = NaN;
-var goalval = NaN;
+var vitalityval = NaN;
+var attunementval = NaN;
 
 //get cookie data
-var saved = cookie.get(['start','goal'],'none');
-if (!isNaN(parseInt(saved.start))) {
-    start_level.value = saved.start;
+var saved = cookie.get(['vitality','attunement'],'none');
+if (!isNaN(parseInt(saved.vitality))) {
+    vitality_goal.value = saved.vitality;
 }
-if (!isNaN(parseInt(saved.goal))) {
-    goal_level.value = saved.goal;
+if (!isNaN(parseInt(saved.attunement))) {
+    attunement_goal.value = saved.attunement;
 }
 
 
-//Update Output when inputs are valid
+//Update Output when some inputs are valid
 setInterval(function () {
     //only check when inputs change
-    if (startval != parseInt(start_level.value) || goalval != parseInt(goal_level.value)) {
-        startval = parseInt(start_level.value);
-        goalval = parseInt(goal_level.value);
+    if (vitalityval != parseInt(vitality_goal.value) || attunementval != parseInt(attunement_goal.value)) {
+        vitalityval = parseInt(vitality_goal.value);
+        attunementval = parseInt(attunement_goal.value);
 
         //save new values to cookie
-        cookie.set('start',startval,{expires:7});
-        cookie.set('goal',goalval,{expires:7});
+        cookie.set('vitality',vitalityval,{expires:7});
+        cookie.set('attunement',attunementval,{expires:7});
 
-        //generate new output if both are ints and goal is higher
-        if (!isNaN(startval) && !isNaN(goalval) && startval < goalval){
-            genOutput(output, startval, goalval);
+        //generate new output if some ar ints, will ignore others
+        if (!isNaN(vitalityval) || !isNaN(attunementval)){
+            genOutput(output);
         } else {
             clearOutput(output);
         }
